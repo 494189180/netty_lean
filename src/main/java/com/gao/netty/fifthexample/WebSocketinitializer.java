05dbb6c9917bd892651d80c4025c5dad36f763af
gaoyanbao <gaoyanbao@csii.com.cn>
2020-07-05 21:50:33 +0800

    netty实现服务端与客户端长连接通信

---
+++ src/main/java/com/gao/netty/fifthexample/WebSocketinitializer.java
@@ -0,0 +1,25 @@
+package com.gao.netty.fifthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class WebSocketinitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new ChunkedWriteHandler());
+        //聚合处理器
+        pipeline.addLast(new HttpObjectAggregator(8192));
+        //处理webSocket繁重的处理
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSocketFrameHandler());
+
+    }
+}
