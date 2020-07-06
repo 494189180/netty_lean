6988edf094fd4be51820b42947fcbb8d5fc7f77b
gaoyanbao <gaoyanbao@csii.com.cn>
2020-07-06 11:48:56 +0800

    netty的protobuf集成netty与多协议消息传递

---
+++ src/main/java/com/gao/netty/sixthexample/TestClientHandler.java
@@ -0,0 +1,18 @@
+package com.gao.netty.sixthexample;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class TestClientHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
+
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        DataInfo.Student student = DataInfo.Student.newBuilder().setName("张三").setAge(22).setAddress("北京").build();
+        ctx.channel().writeAndFlush(student);
+    }
+}
