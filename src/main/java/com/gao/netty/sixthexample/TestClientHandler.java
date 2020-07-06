300126a649533588f70f98ea4fa281100e5ce6c4
gaoyanbao <gaoyanbao@csii.com.cn>
2020-07-06 16:11:50 +0800

    netty的protobuf多协议消息支援与工程实践

--- src/main/java/com/gao/netty/sixthexample/TestClientHandler.java
+++ src/main/java/com/gao/netty/sixthexample/TestClientHandler.java
@@ -3,16 +3,47 @@ package com.gao.netty.sixthexample;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class TestClientHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
+import java.util.Random;
+
+public class TestClientHandler extends SimpleChannelInboundHandler<DataInfo.myMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.myMessage msg) throws Exception {
 
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        DataInfo.Student student = DataInfo.Student.newBuilder().setName("张三").setAge(22).setAddress("北京").build();
-        ctx.channel().writeAndFlush(student);
+        int randomInt = new Random().nextInt(3);
+        System.out.println("randomInt:" + randomInt);
+        DataInfo.myMessage myMessage = null;
+        switch (randomInt) {
+            case 0:
+                myMessage = DataInfo.myMessage.newBuilder().setDataType(DataInfo.myMessage.DataType.PersonType)
+                        .setPerson(DataInfo.Person.newBuilder()
+                                .setName("张一")
+                                .setAge(23)
+                                .setAddress("朝阳门外")
+                                .build())
+                        .build();
+                break;
+            case 1:
+                myMessage = DataInfo.myMessage.newBuilder().setDataType(DataInfo.myMessage.DataType.DogType)
+                        .setDog(DataInfo.Dog.newBuilder()
+                                .setName("小黑")
+                                .setAge(3)
+                                .build())
+                        .build();
+                break;
+            default:
+                myMessage = DataInfo.myMessage.newBuilder().setDataType(DataInfo.myMessage.DataType.CatType)
+                        .setCat(DataInfo.Cat.newBuilder()
+                                .setName("小黑")
+                                .setCity(3)
+                                .build())
+                        .build();
+                break;
+        }
+        ctx.channel().writeAndFlush(myMessage);
     }
 }
--- src/main/java/com/gao/netty/sixthexample/TestServerHandler.java
+++ src/main/java/com/gao/netty/sixthexample/TestServerHandler.java
@@ -3,12 +3,32 @@ package com.gao.netty.sixthexample;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
+public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.myMessage> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
-        System.out.println(msg.getName());
-        System.out.println(msg.getAge());
-        System.out.println(msg.getAddress());
+    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.myMessage msg) throws Exception {
+        DataInfo.myMessage.DataType dataType = msg.getDataType();
+        System.out.println(dataType);
+        switch (dataType) {
+            case PersonType:
+                System.out.println("PersonType:");
+                DataInfo.Person person = msg.getPerson();
+                System.out.println(person.getName());
+                System.out.println(person.getAge());
+                System.out.println(person.getAddress());
+                break;
+            case DogType:
+                System.out.println("DogType:");
+                DataInfo.Dog dog = msg.getDog();
+                System.out.println(dog.getName());
+                System.out.println(dog.getAge());
+                break;
+            case CatType:
+                System.out.println("CatType:");
+                DataInfo.Cat cat = msg.getCat();
+                System.out.println(cat.getName());
+                System.out.println(cat.getCity());
+                break;
+        }
     }
 }
